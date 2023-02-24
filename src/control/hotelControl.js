1d0b6c3485528791e5566004d41a2aea38f418b7
Rojas822 <rojascristian822@gmail.com>
2023-02-24 10:59:25 -0500

    main

--- src/control/hotelControl.js
+++ src/control/hotelControl.js
@@ -1,27 +1,22 @@
 const control = {};
 const axios = require ('axios');
+
 control.list=(req,res) =>{
     req.getConnection((err,conn)=>{
         conn.query('SELECT * FROM hoteles',(err,hoteles)=>{
             if(err){
-               res.json(err); 
+                res.json(err);
             }
-            
-            res.render('index',{data:hoteles});
+            //https://drive.google.com/file/d/1jAg7eHVCgrkmmLPGcw5R46jjUmuOYl0n/view?usp=share_link
+            const url = "https://drive.google.com/uc?export=download&id=1jAg7eHVCgrkmmLPGcw5R46jjUmuOYl0n";
+            axios.get(url).then(response => {
+                let drive = response.data;
+                res.render('index', { data: hoteles, imp: drive });
+            }).catch(error => {
+                console.log(error);
+            });
         });
     });
 };
 
-control.index=(req,res)=>{
-    const url = "https://drive.google.com/uc?export=download&id=1XFzMDmR7R68b52G7LpXXfZVjIlyiI63r";
-    axios.get(url)
-        .then(response => {
-            const drive = response.data;
-            console.log(drive);
-            res.render('index', { data:drive });
-        })
-        .catch(error => {
-            console.log(error);
-        });
-};
     module.exports=control;
